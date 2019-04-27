62b91f139ee8fd6cc016e76b207f7b9f8b4caea5
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2019-04-27 14:39:13 +0530

    SI: Changed the spring boot application to be runnable as a WAR file from any web container.

--- src/main/java/com/swapnil/hospihims/app/Application.java
+++ src/main/java/com/swapnil/hospihims/app/Application.java
@@ -3,6 +3,8 @@ package com.swapnil.hospihims.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,7 +13,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ImportResource({"classpath:applicationContext.xml"})
 @ComponentScan("com.swapnil.hospihims")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
     }
