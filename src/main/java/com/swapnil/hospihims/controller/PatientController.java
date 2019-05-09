21bd67813f20429a64cbe1424920b24f2d4a9e6d
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2019-05-09 10:40:33 +0530

    SI: Added Swagger API dependencies and used this to create documentation for REST APIs and Entity Models. To view the live documentation visit 'http://<IP>/swagger-ui.html#/'

--- src/main/java/com/swapnil/hospihims/controller/PatientController.java
+++ src/main/java/com/swapnil/hospihims/controller/PatientController.java
@@ -1,23 +1,31 @@
 package com.swapnil.hospihims.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.swapnil.hospihims.entity.Patient;
 import com.swapnil.hospihims.service.PatientService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/patient")
+@Api(value = "Hospital management system", description = "Operations pertaining to a patient in Hospital management system")
 public class PatientController {
 	@Autowired
 	private PatientService patientSvc;
 	
-    @RequestMapping("/get")
-    public Patient getPatient(@RequestParam(value="id") int id) {
+	@ApiOperation(value = "Get a patient by Id")
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public Patient getPatient(
+    		@ApiParam(value = "Patient Id from which patient object will be retrieved", required = true)
+    		@PathVariable(value="id") int id) {
     	try {
     		return patientSvc.getPatientById(id);
     	} catch (Exception e) {
@@ -28,8 +36,11 @@ public class PatientController {
     	return null;
     }
 	
-	@RequestMapping(method= RequestMethod.POST, value = "/add")
-	public Patient addPatient(@RequestBody Patient patient) {
+	@ApiOperation(value = "Add a patient")
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public Patient addPatient(
+			@ApiParam(value = "Patient object to be stored in the database", required = true)
+			@RequestBody Patient patient) {
 		try {
 			patientSvc.savePatient(patient);
 			return patient;
