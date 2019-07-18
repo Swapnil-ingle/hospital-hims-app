242a2d8eef2a2c5696f261b2e91da554e7b6c5a3
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2019-07-18 10:29:43 +0530

    SI: Added a utility class 'DateUtils'.

---
+++ src/main/java/com/swapnil/hospihims/utils/DateUtils.java
@@ -0,0 +1,21 @@
+package com.swapnil.hospihims.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateUtils {
+	private DateUtils() {
+		// Private constructor to block object initialization 
+	}
+	
+	private final static String DEF_TIMESTAMP_FORMAT = "HHmmss_ddMMyyyy";
+	
+	public static String getCurrentTimeStamp() {
+		return getCurrentTimeStamp(DEF_TIMESTAMP_FORMAT);
+	}
+	
+	public static String getCurrentTimeStamp(String timestampFormat) {
+		SimpleDateFormat sdf = new SimpleDateFormat(timestampFormat);
+		return sdf.format(Calendar.getInstance().getTime());
+	}
+}
